f2ec0e1b4817bccfecaf43f2a8c68f56eba6018d
MyPc <brianw119@outlook.com>
2024-04-21 22:22:25 +0100

    Code from script file erased, and I restarted from the beginning

--- script.js
+++ script.js
@@ -1,108 +1,63 @@
-let bucket = new Array(16);
-let index = null;
-const person = [];
-let totalEntries = 0;
-let indexLength = 0;
-const firstName = document.querySelector("#first");
-const lastName = document.querySelector("#second");
-const telephone = document.querySelector("#telephone");
-const output = document.querySelector("#display");
+const contacts = {
+   firstName: document.querySelector("#first"),
+   lastName: document.querySelector("#second"),
+   telephone: document.querySelector("#number"),
+   enter: document.querySelector("#enter"),
+   bucketSize: null,
+   details: [],
+   input: [],
+   key: null,
+   inderx: null,
 
-//fields,buttons and display for first name search
-const search = document.querySelector("find");
-const clear = document.querySelector("#clear");
-const display = document.querySelector("#display");
-const name = document.querySelector("#query");
+   bucketCreate(size) {
+      this.bucketSize = size;
+      const bucket = new Array(size);
+      console.log(bucket);
+   },
 
-// converts first name into hash code
-function hash(firstName) {
-  let code = 0;
-  for (let i = 0; i < firstName.length; i++) {
-    code += firstName.charCodeAt(i);
-  }
-  console.log(code % bucket.length);
-  index = code % bucket.length;
-}
-
-// node that contains contact details
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-  }
-}
-
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.length = 0;
-  }
-  //sets contact details
-  addNode(first, last, number) {
-    //
-    const position = bucket[index];
-    const data = [first, last, number];
-    const node = new Node(data);
-    indexLength++;
-    if (position.head === null) {
-      position.head = node;
-    } else {
-      let current = position.head;
-      while (current.next !== null) {
-        current = current.next;
+   hash(first) {
+      // index = null;
+      let code = 0;
+      for (let i = 0; i < first.length; i++) {
+         code += first.charCodeAt(i);
       }
-      current.next = node;
-    }
-    this.length = indexLength;
-    console.log(`length: ${this.length}`);
-  }
+      //console.log(code % this.bucketSize);
+      return code % this.bucketSize;
+   },
 
-  //clear all nodes at specified index in bucket
-  clearNodes() {
-    const position = bucket[index];
-    position.head = null;
-    position.next = null;
-    this.length = 0;
-  }
+   capture() {
+      enter.addEventListener("click", () => {
+         this.details = [];
+         this.details.push(
+            this.firstName.value,
+            this.lastName.value,
+            this.telephone.value
+         );
+         console.log(this.details);
+      });
+   },
 
-  //returns size of linked list at index
-  indexSize() {
-    console.log(this.length);
-  }
+   generateKey() {
+      this.key = null;
+      this.enter.addEventListener("click", () => {
+         this.key = this.details[0];
+         console.log(`key = ${this.key}`);
+      });
+   },
 
-  clearAllContacts() {
-    for (let i = 0; i < bucket.length; i++) {
-      if (bucket[i].head !== null) {
-        this.clearNodes();
-      }
-    }
-    this.length = 0;
-  }
-  //
-}
+   generateIndex() {
+      this.enter.addEventListener("click", () => {
+         this.index = this.hash(this.key);
+         console.log(`index = ${this.index}`);
+      });
+   },
 
-//inserts a blank linked list at each index of array on page load
-function insertBlanks() {
-  const blank = new LinkedList();
-  for (let i = 0; i < bucket.length; i++) {
-    // console.log(blank);
-    bucket[i] = blank;
-  }
-}
+   init() {
+      this.bucketCreate(16);
+      this.capture();
+      this.generateKey();
+      this.generateIndex();
+   },
+};
 
-insertBlanks();
-hash("hss");
-console.log(`index: ${index}`);
-console.log(bucket);
-let newData = new LinkedList();
-newData.addNode("hdrjrrk", "ward", "0988888");
-newData.addNode("dsfsgs", "hshjshs", "02555");
-newData.addNode("abcgde", "skdkd", "123456");
-newData.addNode("durgbdf", "ofunf", "93646");
-newData.addNode("durgbdf", "ofunf", "93646");
-//newData.clearAllContacts();
-newData.indexSize();
-newData.clearAllContacts();
-newData.indexSize();
-//console.log(newData);
-//console.log(bucket[index].head);
+contacts.init();
--- script2.js
+++ script2.js
@@ -1,88 +1,94 @@
 const myContacts = {
-  //fields and buttons for adding new contacts
-  firstName: document.querySelector("#first"),
-  lastName: document.querySelector("#second"),
-  telephone: document.querySelector("#number"),
-  output: document.querySelector("#display"),
+   //fields and buttons for adding new contacts
+   firstName: document.querySelector("#first"),
+   lastName: document.querySelector("#second"),
+   telephone: document.querySelector("#number"),
+   output: document.querySelector("#display"),
 
-  //fields,buttons and display for first name search
-  search: document.querySelector("find"),
-  clear: document.querySelector("#clear"),
-  display: document.querySelector("#display"),
-  name: document.querySelector("#query"),
+   //fields,buttons and display for first name search
+   search: document.querySelector("find"),
+   clear: document.querySelector("#clear"),
+   display: document.querySelector("#display"),
+   name: document.querySelector("#query"),
 
-  bucket: new Array(16),
-  index: null,
+   bucket: new Array(16),
+   index: null,
 
-  //converts first name to hash code
-  hash(firstName) {
-    let code = 0;
-    for (let i = 0; i < firstName.length; i++) {
-      code += firstName.charCodeAt(i);
-    }
-    console.log(code % bucket.length);
-    index = code % bucket.length;
-  },
-
-  node: class Node {
-    constructor(data) {
-      this.data = data;
-      this.next = null;
-    }
-  },
-
-  list: class LinkedList {
-    constructor() {
-      this.head = null;
-      this.length = 0;
-    }
-    //sets contact details
-    addName(first, last, number) {
-      const position = bucket[index];
-      const data = [first, last, number];
-      const node = new Node(data);
-      if (position.head === null) {
-        position.head = node;
-      } else {
-        let current = position.head;
-        while (current.next !== null) {
-          current = current.next;
-        }
-        current.next = node;
+   //converts first name to hash code
+   hash(firstName) {
+      let code = 0;
+      for (let i = 0; i < firstName.length; i++) {
+         code += firstName.charCodeAt(i);
       }
-      this.length++;
-    }
+      console.log(code % bucket.length);
+      index = code % bucket.length;
+   },
 
-    //clear all nodes at specified index in bucket
-    clearNodes() {
-      const position = bucket[index];
-      position.head = null;
-      position.next = null;
-      this.length = 0;
-    }
+   node: class Node {
+      constructor(data) {
+         this.data = data;
+         this.next = null;
+      }
+   },
 
-    //returns size of linked list at index
-    indexSize() {
-      console.log(this.length);
-    }
+   list: class LinkedList {
+      constructor() {
+         this.head = null;
+         this.length = 0;
+      }
+      //sets contact details
+      set(first, last, number) {
+         const position = bucket[index];
+         const data = [first, last, number];
+         const node = new Node(data);
+         if (position.head === null) {
+            position.head = node;
+         } else {
+            let current = position.head;
+            while (current.next !== null) {
+               current = current.next;
+            }
+            current.next = node;
+         }
+         this.length++;
+      }
 
-    clearAllContacts() {
+      //clear all nodes at specified index in bucket
+      clearNodes() {
+         const position = bucket[index];
+         position.head = null;
+         position.next = null;
+         this.length = 0;
+      }
+
+      //returns size of linked list at index
+      indexSize() {
+         console.log(this.length);
+      }
+
+      clearAllContacts() {
+         for (let i = 0; i < bucket.length; i++) {
+            if (bucket[i].head !== null) {
+               this.clearNodes();
+            }
+         }
+      }
+   },
+
+   //initial condition
+   startCondition() {
+      let count = 0;
       for (let i = 0; i < bucket.length; i++) {
-        if (bucket[i].head !== null) {
-          this.clearNodes();
-        }
+         count++;
       }
-    }
-  },
+      if (count === bucket.length) {
+         insertBlanks();
+      }
+   },
 
-  insertBlanks() {
-    const blank = new linkedList();
-    for (let i = 0; i < bucket.length; i++) {
-      bucket[i] = blank;
-    }
-  },
-
-  init() {},
+   init() {
+      startCondition();
+   },
 };
 
 myContacts.init();
